448d4bfe019a30c6f0e7b2e83157fa817f336aae
Superstarlex <98848687+Superstarlex@users.noreply.github.com>
2022-04-20 13:55:13 -0600

    Create antireload.js

---
+++ user_scripts/antireload.js
@@ -0,0 +1,9 @@
+var changes = false;        
+    window.onbeforeunload = function() {
+        if (changes)
+        {
+            var message = "Are you sure you want to navigate away from this page?\n\nYou have started writing or editing a post.\n\nPress OK to continue or Cancel to stay on the current page.";
+            if (confirm(message)) return true;
+            else return false;
+        }
+    }
