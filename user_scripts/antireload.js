c40870026e8ddd96ff7f0a853cde42ca4973bed7
Superstarlex <98848687+Superstarlex@users.noreply.github.com>
2022-04-20 14:11:30 -0600

    Update antireload.js

--- user_scripts/antireload.js
+++ user_scripts/antireload.js
@@ -1,9 +1,5 @@
-var changes = false;        
 window.onbeforeunload = function() {
-   if (changes)
-   {
-      var message = "Are you sure you want to navigate away from this page?\n\nYou have started writing or editing a post.\n\nPress OK to continue or Cancel to stay on the current page.";
-      if (confirm(message)) return true;
-      else return false;
-   }
-}
+    return true;
+};
+// Remove navigation prompt
+window.onbeforeunload = null;
