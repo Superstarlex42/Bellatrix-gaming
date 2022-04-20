fdb3dca41a7e21421aa77a294e95bfccdc611643
Superstarlex <98848687+Superstarlex@users.noreply.github.com>
2022-04-20 15:04:31 -0600

    Update noreload.js

--- user_scripts/noreload.js
+++ user_scripts/noreload.js
@@ -1,14 +1,3 @@
-const beforeUnloadListener = (event) => {
-  event.preventDefault();
-  return event.returnValue = "Are you sure you want to exit?";
-};
-
-const nameInput = document.querySelector("#name");
-
-nameInput.addEventListener("input", (event) => {
-  if (event.target.value !== "") {
-    addEventListener("beforeunload", beforeUnloadListener, {capture: true});
-  } else {
-    removeEventListener("beforeunload", beforeUnloadListener, {capture: true});
-  }
+window.addEventListener("unload", function () {
+  parent.console.log("reload?")
 });
