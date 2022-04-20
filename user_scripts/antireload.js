50583ec4faa5e8d3e7f2f78d60e7f801b7512b2b
Superstarlex <98848687+Superstarlex@users.noreply.github.com>
2022-04-20 11:50:40 -0600

    Update antireload.js

--- user_scripts/antireload.js
+++ user_scripts/antireload.js
@@ -1,1 +1,16 @@
+var formSubmitting = false;
+var setFormSubmitting = function() { formSubmitting = true; };
 
+window.onload = function() {
+    window.addEventListener("beforeunload", function (e) {
+        if (formSubmitting) {
+            return undefined;
+        }
+
+        var confirmationMessage = 'Leave Site?'
+                                + 'You may lose unsaved changes';
+        
+        (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+        return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+    });
+};
