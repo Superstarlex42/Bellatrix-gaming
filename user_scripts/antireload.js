9a095a96b1f3374232cee9e07bd68d41cc577d68
Superstarlex <98848687+Superstarlex@users.noreply.github.com>
2022-04-20 14:48:04 -0600

    Update antireload.js

--- user_scripts/antireload.js
+++ user_scripts/antireload.js
@@ -1,5 +1,7 @@
-window.onbeforeunload = function() {
-    return true;
-};
-// Remove navigation prompt
-window.onbeforeunload = null;
+window.addEventListener("beforeunload", function (e) {
+    var confirmationMessage = 'It looks like you have been editing something. '
+                            + 'If you leave before saving, your changes will be lost.';
+
+    (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+    return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+});
