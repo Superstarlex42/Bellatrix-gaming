20a4d8747b44df081e03e1f0ce7a5bfafa0fbde3
Superstarlex <98848687+Superstarlex@users.noreply.github.com>
2022-04-21 06:40:41 -0600

    Update noreload.js

--- user_scripts/noreload.js
+++ user_scripts/noreload.js
@@ -1,4 +1,4 @@
-window.beforeunload = function(e) {
+window.onbeforeunload = function(e) {
   var dialogText = 'We are saving the status of your listing. Are you realy sure you want to leave?';
   e.returnValue = dialogText;
   return dialogText;
